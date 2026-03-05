73f635cbad5375ceea385e932279fa7624a7cc81
Yoseinoo <92569285+Yoseinoo@users.noreply.github.com>
2026-03-05 09:31:07 +0100

    feat: architecture modulaire (5 modules) + Docker Compose + ConfigModule

--- taskflow-api/src/app.module.ts
+++ taskflow-api/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { TeamsModule } from './teams/teams.module';
+import { ProjectsModule } from './projects/projects.module';
+import { TasksModule } from './tasks/tasks.module';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    UsersModule,
+    TeamsModule,
+    ProjectsModule,
+    TasksModule,
+    CommentsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
